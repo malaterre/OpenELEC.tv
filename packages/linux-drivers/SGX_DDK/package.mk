84ca1b3293bc6558ee4e494ce8c6f46a0359916c
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2017-04-24 21:10:46 +0200

    fix up SGX

--- packages/linux-drivers/SGX_DDK/package.mk
+++ packages/linux-drivers/SGX_DDK/package.mk
@@ -15,34 +15,28 @@
 #  You should have received a copy of the GNU General Public License
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
-set -x
-#PKG_NAME="CI20-SGX"
-PKG_NAME="SGX_DDK_Linux_XOrg_MAIN@3759903_source_km"
+
+PKG_NAME="SGX_DDK"
 PKG_VERSION="3759903"
 PKG_REV="1"
 PKG_ARCH="mips"
 PKG_LICENSE="GPL"
 PKG_SITE="http://elinux.org/CI20-SGX_kernel_module"
-#PKG_URL="http://mipscreator.imgtec.com/CI20/sgx/SGX_DDK_Linux_XOrg_MAIN%403759903_source_km.tgz"
-PKG_URL="http://mipscreator.imgtec.com/CI20/sgx/SGX_DDK_Linux_XOrg_MAIN@3759903_source_km.tgz"
-#PKG_URL="http://mipscreator.imgtec.com/CI20/sgx/${PKG_NAME}[@]${PKG_VERSION}_source_km.tgz"
+PKG_URL="http://mipscreator.imgtec.com/CI20/sgx/SGX_DDK_Linux_XOrg_MAIN@${PKG_VERSION}_source_km.tgz"
+PKG_SOURCE_DIR="eurasia_km"
 PKG_DEPENDS_TARGET="toolchain linux"
-#PKG_NEED_UNPACK="$LINUX_DEPENDS"
+PKG_NEED_UNPACK="$LINUX_DEPENDS"
 PKG_PRIORITY="optional"
 PKG_SECTION="driver"
-PKG_SHORTDESC="CI20-SGX"
-PKG_LONGDESC="CI20-SGX"
+PKG_SHORTDESC="CI20-SGX bla"
+PKG_LONGDESC="CI20-SGX foo"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-pre_make_target() {
-  unset LDFLAGS
-}
-
 # http://elinux.org/CI20-SGX_kernel_module
 make_target() {
-  CROSS_COMPILE=${TARGET_NAME} ARCH=$TARGET_KERNEL_ARCH KERNELDIR=/home/mathieu/tmp/OpenELEC.tv/build.OpenELEC-ci20.mips-8.0-devel/linux-3c61f45 make -C eurasiacon/build/linux2/jz4780_linux V=1
+  CROSS_COMPILE=${TARGET_NAME}- ARCH=$TARGET_KERNEL_ARCH KERNELDIR=/home/mathieu/tmp/OpenELEC.tv/build.OpenELEC-ci20.mips-8.0-devel/linux-3c61f45 make -C eurasiacon/build/linux2/jz4780_linux V=1
 #  make V=1 \
 #       ARCH=$TARGET_KERNEL_ARCH \
 #       KSRC=$(get_pkg_build linux) \
@@ -52,5 +46,5 @@ make_target() {
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/modules/$(get_module_dir)/$PKG_NAME
-    cp *.ko $INSTALL/usr/lib/modules/$(get_module_dir)/$PKG_NAME
+    cp eurasiacon/binary2_jz4780_linux_release/target/pvrsrvkm.ko $INSTALL/usr/lib/modules/$(get_module_dir)/$PKG_NAME
 }
